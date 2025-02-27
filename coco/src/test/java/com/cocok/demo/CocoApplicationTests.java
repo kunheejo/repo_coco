8421b9a153647d30f082131c3757558e6de7b7ca
kunheejo <whrjsgml0901@naver.com>
2025-02-27 12:07:54 +0900

    2/27

---
+++ coco/src/test/java/com/cocok/demo/CocoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocok.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
